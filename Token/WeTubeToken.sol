0914bb67ae0d2da7378aacd63956b09cea71f2d5
devinlee <e1132334@u.nus.edu>
2023-12-09 10:02:02 +0800

    switch to ERC721

--- Token/WeTubeToken.sol
+++ Token/WeTubeToken.sol
@@ -1,30 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WeTubeToken is ERC20 {
-    address public owner;
+contract WeTubeNFT is ERC721Enumerable, Ownable {
+    uint256 public nextTokenId = 1;
+    uint256 public tokenPrice;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner");
-        _;
+    event TokenPurchased(address indexed supporter, uint256 indexed tokenId);
+
+    // Constructor initializes the ERC721 contract and sets the token price
+    constructor(string memory name, string memory symbol, uint256 _tokenPrice) ERC721(name, symbol) {
+        tokenPrice = _tokenPrice;
     }
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _initialSupply
-    ) ERC20(_name, _symbol) {
-        owner = msg.sender;
-        _mint(msg.sender, _initialSupply);
+    // Mint new NFTs and assign them to a specified address
+    function mint(address to) external onlyOwner {
+        // Ensure the token limit is not exceeded
+        require(nextTokenId <= 10000, "Token limit reached");
+
+        // Mint a new NFT and assign it to the specified address
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
     }
 
-    function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
-    }
+    // Purchase NFTs by sending the required amount of Ether
+    function purchaseToken() external payable {
+        // Ensure the correct amount of Ether is sent
+        require(msg.value == tokenPrice, "Invalid pledge amount");
 
-    function burn(address account, uint256 amount) external onlyOwner {
-        _burn(account, amount);
+        // Mint a new NFT and assign it to the supporter
+        _safeMint(msg.sender, nextTokenId);
+        nextTokenId++;
+
+        // Send Ether to the platform owner
+        payable(owner()).transfer(msg.value);
+
+        // Emit an event to record the NFT purchase
+        emit TokenPurchased(msg.sender, nextTokenId - 1);
     }
 }
